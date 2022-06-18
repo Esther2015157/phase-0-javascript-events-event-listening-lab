81c895af9daed26012780663cd35574493866bec
Esther2015157 <esthersly8@gmail.com>
2022-06-18 14:32:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+function addingEventListener() {const input = document.getElementById('input');
+
+function clickAlert() {
+    alert('Hee hee, iwas tickled');
+    alert('I was clicked');
+}
+
+input.addEventListener('click', clickAlert);
+
 }
